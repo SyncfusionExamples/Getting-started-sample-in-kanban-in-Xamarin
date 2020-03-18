102d8639ae81bb1824fce57dd2047556eb76fd96
AzureAD\RamyaSoundarRajan <ramya.soundarrajan@syncfusion.com>
2020-03-18 17:06:28 +0530

    Created a getting started sample for kanban.

---
+++ GettingStartedKanban/GettingStartedKanban/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.SfKanban.XForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GettingStartedKanban
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            openColumn.Categories = new List<object>() { "Open" };
+            progressColumn.Categories = new List<object>() { "In Progress" };
+            codeColumn.Categories = new List<object>() { "Code Review" };
+            doneColumn.Categories = new List<object>() { "Done" };
+        }
+    }
+}
---
+++ GettingStartedKanban/GettingStartedKanban/ViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Syncfusion.SfKanban.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace GettingStartedKanban
+{
+    public class ViewModel
+    {
+        public ObservableCollection<KanbanModel> Cards { get; set; }
+        public ViewModel()
+        {
+            Cards = new ObservableCollection<KanbanModel>();
+            Cards.Add(new KanbanModel()
+            {
+                ID = 1,
+                Title = "iOS - 1002",
+                ImageURL = "People_Circle1.png",
+                Category = "Open",
+                Description = "Analyze customer requirements",
+                ColorKey = "Red",
+                Tags = new string[] { "Incident", "Customer" }
+            });
+            Cards.Add(new KanbanModel()
+            {
+                ID = 6,
+                Title = "Xamarin - 4576",
+                ImageURL = "People_Circle2.png",
+                Category = "Open",
+                Description = "Show the retrieved data from the server in grid control",
+                ColorKey = "Green",
+                Tags = new string[] { "Story", "Customer" }
+            });
+            Cards.Add(new KanbanModel()
+            {
+                ID = 13,
+                Title = "UWP - 13",
+                ImageURL = "People_Circle3.png",
+                Category = "In Progress",
+                Description = "Add responsive support to application",
+                ColorKey = "Brown",
+                Tags = new string[] { "Story", "Customer" }
+            });
+            Cards.Add(new KanbanModel()
+            {
+                ID = 2543,
+                Title = "Xamarin_iOS - 2543",
+                Category = "Code Review",
+                ImageURL = "People_Circle4.png",
+                Description = "Check login page validation",
+                ColorKey = "Brown",
+                Tags = new string[] { "Story", "Customer" }
+            });
+        }
+    }
+
+}
